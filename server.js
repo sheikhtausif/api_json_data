d79fd96d9605fa22f78f5c478227dfff800b7fb2
sheikhtausif <mohammadtausif14@gmail.com>
2023-01-28 23:18:58 +0530

    add route

--- server.js
+++ server.js
@@ -1,10 +1,19 @@
 const jsonServer = require('json-server');
+const express = require('express');
 const server = jsonServer.create();
 const router = jsonServer.router('db.json');
 const middleware = jsonServer.defaults();
 const port = process.env.PORT || 5000;
+const app = express();
+app.use(express.json())
 
 server.use(middleware);
 server.use(router);
 
-server.listen(port);
+app.get('/', (req, res) => {
+    res.send("API is running...")
+})
+
+server.listen(port, () => {
+    console.log("server is running...")
+});
